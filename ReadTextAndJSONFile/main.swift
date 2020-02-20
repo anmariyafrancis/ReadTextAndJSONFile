524313527ed68b35fec65089eeb67d884820802b
anmariyafrancis <anmariya1915@gmail.com>
2020-02-19 19:03:25 -0500

    jsonfile reading

--- ReadTextAndJSONFile/main.swift
+++ ReadTextAndJSONFile/main.swift
@@ -8,18 +8,47 @@
 
 import Foundation
 
-func readTextFile(fileName:String)
+/*func readTextFile(fileName:String)
 {
     print(Bundle.main.bundlePath)
     if let path=Bundle.main.path(forResource: fileName, ofType: "txt")
     {
         print(path)
-        let str=try!String(contentsOfFile: path)
+        let str=try!String(contentsOfFile: path) //can't return nil 
         print(str)
     }
     
     
 }
 
-readTextFile(fileName: "data")
+readTextFile(fileName: "data")*/
+
+func readJSONFile(jsonFileName:String)
+{
+    let filePath=Bundle.main.url(forResource: jsonFileName, withExtension: "json")
+    guard let path=filePath else {
+        print("Inavalid file path found")
+        return
+    }
+    guard let data=try? Data(contentsOf: path) else {
+        print("Error while data read from URL")
+        return
+    }
+    guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+        print("Error while data read from file")
+               return
+    }
+    //print(json)
+    
+   if let userDictionary=json as? [String:Any]
+   {
+        print(userDictionary)
+        let id=userDictionary["id"] ?? "No ID Found"
+                     print(id)
+        
+       
+    }
+}
+
+readJSONFile(jsonFileName: "user")
 
